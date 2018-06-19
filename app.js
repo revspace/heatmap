f7f4aaac248ad7cf3716ded7d26bea590ee2078e
f0x <f@0x52.eu>
2018-06-20 00:24:14 +0200

    Animated year slider

--- app.js
+++ app.js
@@ -14,42 +14,50 @@ let errorReporter = unhandledError((error, context) => {
 
 let app = express();
 let router = expressPromiseRouter();
-let now = new Date();
-const currentYear = `year${now.getFullYear()}`;
-const weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-const fileNames = {
-  [currentYear]: "This Year",
-  "alltime": "All Time",
-  "7days": "Last 7 Days",
-  "70days": "Last 70 Days",
-  "365days": "Last 365 Days"
-};
-
 router.get("/", function(req, res){
+  let now = new Date();
+  const currentYear = `year${now.getFullYear()}`;
+  const weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const fileNames = {
+    "alltime": "All Time",
+    "7days": "Last 7 Days",
+    "70days": "Last 70 Days",
+    "365days": "Last 365 Days"
+  };
+
   return Promise.try(function(){
-    for (var year=2010; year < now.getFullYear(); year++) {
-      fileNames[`year${year}`] = year.toString();
+    let yearFileNames = {};
+    for (var year=2010; year <= now.getFullYear(); year++) {
+      yearFileNames[`year${year}`] = year.toString();
     }
 
-    return Promise.map(Object.keys(fileNames), (fileName) => {
-      return getHeatmap(fileName);
+    let normalHeatmaps = Promise.map(Object.keys(fileNames), (fileName) => {
+      return getHeatmap(fileName, weekDays, fileNames);
     });
-  }).then((heatmaps) => {
-    let shiftedHeatmapDatas = [];
-    heatmaps.forEach((heatmapData) => {
-      let originalHeatmap = heatmapData.heatmap;
-      let shiftedHeatmap = originalHeatmap;
-      //Move Sunday to end of array
-      shiftedHeatmap.push(shiftedHeatmap.shift());
-      shiftedHeatmapDatas.push({header: heatmapData.header, heatmap: shiftedHeatmap});
-    })
-    console.log(shiftedHeatmapDatas);
-    let data = {heatmaps: shiftedHeatmapDatas};
-    res.render('index', data);
+
+    let yearHeatmapSlider = Promise.map(Object.keys(yearFileNames), (fileName) => {
+      return getHeatmap(fileName, weekDays, yearFileNames);
+    });
+
+    return Promise.all([
+      normalHeatmaps,
+      yearHeatmapSlider
+    ]);
+  }).then((data) => {
+    let normalHeatmaps = data[0];
+    let yearHeatmapSlider = data[1];
+
+    let yearSlider = {
+      firstYear: 2010,
+      lastYear: now.getFullYear(),
+      data: yearHeatmapSlider
+    };
+
+    res.render('index', {heatmaps: normalHeatmaps, yearSlider: yearSlider});
   });
 });
 
-function getHeatmap(filename) {
+function getHeatmap(filename, weekDays, fileNames) {
   return readFile(`heatmaps/${filename}.json`, 'ascii')
     .then((file) => JSON.parse(file))
     .then((json) => {
@@ -65,6 +73,9 @@ function getHeatmap(filename) {
         let columns = [];
         values.forEach((percentage) => {
           let color = `hsla(${1.2*percentage}, 100%, 50%, 1)`;
+          if (percentage == '100' && (Math.random() * 100) < 2) {
+            percentage = 108;
+          }
           columns.push({color: color, percentage: percentage.toString()}); //nbsp
         });
 
@@ -74,7 +85,11 @@ function getHeatmap(filename) {
         });
       });
 
-      return {header: fileNames[filename], heatmap: heatmap};
+      //Move Sunday to end of array
+      let shiftedHeatmap = heatmap;
+      shiftedHeatmap.push(shiftedHeatmap.shift());
+
+      return {header: fileNames[filename], heatmap: shiftedHeatmap, filename: filename};
     })
 }
 
